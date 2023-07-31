25295fb678ed6a36c9ebbc005ddf2640951ec9c6
Pethum Jeewantha <pethumjeewantha1@gmail.com>
2023-07-31 16:37:25 +0530

    add comments

--- notepad.c
+++ notepad.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <ncurses.h>
 
-// Define a macro for the maximum buffer size
+// Maximum buffer size
 #define MAX_BUFFER_SIZE 5000
 
 // Function to correct the capitalization in a string
@@ -14,12 +14,8 @@ void correctCapitalization(char *str)
     {
         if (newSentence && str[i] >= 'a' && str[i] <= 'z')
         {
-            str[i] -= 'a' - 'A';  // Convert the first letter of a sentence to uppercase
+            str[i] -= 'a' - 'A'; // Convert the first letter of a sentence to uppercase
             newSentence = 0;
-        }
-        else if (!newSentence && str[i] >= 'A' && str[i] <= 'Z')
-        {
-            str[i] += 'a' - 'A';  // Convert non-first letters to lowercase
         }
 
         // Set the flag for a new sentence if a period (.) is found followed by a space or end of string,
@@ -31,20 +27,27 @@ void correctCapitalization(char *str)
     }
 }
 
-// Function to count the words in a string
-int countWords(const char *str)
+// Function to search
+void search(const char *text, const char *term)
 {
-    int count = 0;
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-        // Increment the word count for each space found that is followed by a non-space character
-        if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
-        {
-            count++;
-        }
-    }
+    const char *p = text;
 
-    return count + 1;
+    while ((p = strstr(p, term)) != NULL)
+    {
+        // Calculate the position of the found term
+        int pos = p - text;
+
+        // Move the cursor to the position of the found term
+        move(pos / COLS, pos % COLS);
+
+        // Highlight the found term
+        attron(A_REVERSE);
+        printw("%s", term);
+        attroff(A_REVERSE);
+
+        // Move the pointer past the found term
+        p += strlen(term);
+    }
 }
 
 // Function to open a file and display its content
@@ -60,7 +63,7 @@ void openFile(const char *filename)
     char c;
     while ((c = fgetc(file)) != EOF)
     {
-        addch(c);  // Display the content of the file
+        addch(c); // Display the content of the file
     }
 
     fclose(file);
@@ -76,7 +79,7 @@ void saveFile(const char *filename, const char *content)
         return;
     }
 
-    fputs(content, file);  // Write the content to the file
+    fputs(content, file); // Write the content to the file
 
     fclose(file);
     printw("File saved successfully.\n");
@@ -91,7 +94,7 @@ int main(int argc, char *argv[])
     keypad(stdscr, TRUE); // We get F1, F2, etc.
     noecho();             // Don't echo() while we do getch
 
-    char filename[100];   // Variable to store the filename
+    char filename[100]; // Variable to store the filename
     if (argc > 1)
     {
         strncpy(filename, argv[1], sizeof(filename) - 1); // Copy the filename from command line arguments
@@ -100,9 +103,9 @@ int main(int argc, char *argv[])
     else
     {
         printw("Enter the filename to open (include the extension, e.g., 'file.txt'): ");
-        echo(); // Enable echoing of typed characters
+        echo();           // Enable echoing of typed characters
         getstr(filename); // Get the filename from user input
-        noecho(); // Disable echoing of typed characters
+        noecho();         // Disable echoing of typed characters
     }
 
     openFile(filename); // Open and display the content of the file
@@ -116,16 +119,31 @@ int main(int argc, char *argv[])
     // Main loop to get user input and handle commands
     while ((ch = getch()) != EOF)
     {
-        if (ch == '\n') // If the user enters a newline
+        if (input_len >= 2 && strncmp(input + input_len - 2, ":/", 2) == 0) // Check if the user entered ':/term' to search for 'term'
         {
-            addch(ch);  // Display the newline
-            input[input_len++] = ch;  // Add the newline to the input
+            echo(); // Enable echoing of typed characters
+
+            // Get the search term from user input
+            char term[MAX_BUFFER_SIZE];
+            getstr(term);
+
+            noecho(); // Disable echoing of typed characters
+
+            // Search for the term in the input
+            search(input, term);
+
+            continue; // Skip the rest of the loop iteration
+        }
+        else if (ch == '\n') // If the user enters a newline
+        {
+            addch(ch);               // Display the newline
+            input[input_len++] = ch; // Add the newline to the input
 
             // Check if the user entered ':wq' to save and quit
             if (input_len >= 4 && strcmp(input + input_len - 4, ":wq\n") == 0)
             {
                 input[input_len - 4] = '\0'; // Remove the ':wq\n' from the input
-                break;  // Break the loop to save and quit
+                break;                       // Break the loop to save and quit
             }
             // Check if the user entered ':q' to quit without saving
             else if (input_len >= 3 && strcmp(input + input_len - 3, ":q\n") == 0)
@@ -142,8 +160,8 @@ int main(int argc, char *argv[])
                 input[--input_len] = '\0'; // Erase the last character from the input
                 int y, x;
                 getyx(stdscr, y, x); // Get the current cursor position
-                move(y, x - 1); // Move the cursor back one position
-                delch(); // Delete the character at the cursor position
+                move(y, x - 1);      // Move the cursor back one position
+                delch();             // Delete the character at the cursor position
             }
         }
         else if (ch == KEY_BACKSPACE || ch == 127 || ch == 8) // If the user enters the backspace key
@@ -154,8 +172,8 @@ int main(int argc, char *argv[])
                 input[--input_len] = '\0'; // Backspace the last character from the input
                 int y, x;
                 getyx(stdscr, y, x); // Get the current cursor position
-                move(y, x - 1); // Move the cursor back one position
-                delch(); // Delete the character at the cursor position
+                move(y, x - 1);      // Move the cursor back one position
+                delch();             // Delete the character at the cursor position
             }
         }
         else // If the user enters any other character
@@ -163,7 +181,7 @@ int main(int argc, char *argv[])
             // If there is space in the input buffer
             if (input_len < MAX_BUFFER_SIZE - 1)
             {
-                addch(ch); // Display the character
+                addch(ch);               // Display the character
                 input[input_len++] = ch; // Add the character to the input
             }
         }
@@ -172,7 +190,7 @@ int main(int argc, char *argv[])
     endwin(); // End curses mode
 
     correctCapitalization(input); // Correct the capitalization in the input
-    saveFile(filename, input); // Save the input to the file
+    saveFile(filename, input);    // Save the input to the file
 
     return 0;
 }
